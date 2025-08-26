ad7a7f0778b9461e867ab88ef05e8d6f96751222
alisson0liveir2 <145785362+alisson0liveir2@users.noreply.github.com>
2025-08-26 07:44:18 -0300

    w

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const numeroSenha=document.querySelector('.parametro-senha__texto');
-let tamanhoSenha=12;
-numeroSenha.textContent=tamanhoSenha;
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
 
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 
