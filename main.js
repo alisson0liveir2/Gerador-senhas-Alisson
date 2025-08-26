3df2cea63c7869241767be6bc4eab3fac5c3204e
alisson0liveir2 <145785362+alisson0liveir2@users.noreply.github.com>
2025-08-26 08:44:44 -0300

    26

--- main.js
+++ main.js
@@ -31,12 +31,12 @@ const numeroSenha = '0123456789';
 const simbolos = '!@#$&*?/-';
 geraSenha();
 
-function geraSenha(){
+function geraSenha() {
     let senha = '';
-    for(let i=0; i<tamanhoSenha; i++){
-        let numeroAleatorio = Math.random()*letrasMaiusculas.length;
-    numeroAleatorio = Math.floor(numeroAleatorio);
-    senha = senha + letrasMaiusculas[numeroAleatorio];
+    for (let i = 0; i < tamanhoSenha; i++) {
+        let numeroAleatorio = Math.random() * letrasMaiusculas.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + letrasMaiusculas[numeroAleatorio];
     }
     campoSenha.value = senha;
 }
